cd86c5c00fa570832bed0b9914c26899b34fca6b
nikolajsen121 <130678378+nikolajsen121@users.noreply.github.com>
2023-04-14 13:45:45 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,57 @@
+let hand = 0
+input.onGesture(Gesture.ThreeG, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # . . #
+        # # . . .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (false) {
+        if (hand == 2) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        } else if (hand == 3) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        }
+    }
+})
+basic.forever(function () {
+	
+})
